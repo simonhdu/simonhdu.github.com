a566603eb103272612eba76b0246688ed3e86352
刘建辉 <liujianhui@raycloud.com>
2016-09-29 00:43:40 +0800

    delete

--- mock_ajax/bindCode.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by liujianhui on 16/9/23.
- */
-module.exports = function() {
-    return {
-        result: 100
-    }
-}
--- mock_ajax/updateItem.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by qianyongjun on 16/7/20.
- */
-module.exports = function() {
-  return {
-    result: 100
-  }
-}
